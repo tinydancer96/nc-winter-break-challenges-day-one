217247207d21464b599738364acd2f2f874ee571
tinydancer96 <mardgelleneri@gmail.com>
2025-12-22 22:29:52 +0000

    remove comments tests

--- day-three.js
+++ day-three.js
@@ -1,12 +1,3 @@
-/*
-// Tests
-console.log(uniteUnique([1, 2, 3], [5, 2, 1]), "→ Expected: [1, 2, 3, 5]");
-console.log(uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]), "→ Expected: [1, 2, 3, 5, 4, 6, 7, 8]");
-console.log(uniteUnique([1, 3, 2], [5, 4], [5, 6]), "→ Expected: [1, 3, 2, 5, 4, 6]");
-console.log(uniteUnique([1, 3, 2, 3], [5, 2, 1, 4], [2, 1]), "→ Expected: [1, 3, 2, 5, 4]");
-
-*/
-
 function uniteUnique(...arr) {
   let uniqueItems = [];
   for (let i = 0; i < arr.length; i++) {
